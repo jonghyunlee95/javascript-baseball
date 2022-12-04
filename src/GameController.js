731f4b7751499cf5b207a3956c01484bf8a64a52
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-12-04 17:07:15 +0900

    feat: 중복검사 추가 구현

--- src/GameController.js
+++ src/GameController.js
@@ -1,9 +1,10 @@
-const validation = {
+const GameController = {
   validateUserInput(userInput) {
     if (userInput.length !== 3) {
       throw new Error('3자리 수를 입력해 주세요.');
     }
     this.validateNumber(userInput);
+    this.validateDuplicate(userInput);
   },
 
   validateNumber(userInput) {
@@ -16,5 +17,12 @@ const validation = {
       }
     }
   },
+
+  validateDuplicate(userInput) {
+    const NOTDUPLICATE = new Set(userInput);
+    if (userInput.length !== NOTDUPLICATE.length) {
+      throw new Error('중복되지 않는 숫자를 입력해 주세요.');
+    }
+  },
 };
-module.exports = validation;
+module.exports = GameController;
